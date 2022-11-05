4b98b629b03b398b1dd398feb74c81c569ae4f25
Courtney Stow <courney-stow@hotmail.co.uk>
2022-11-05 17:51:44 +0000

    Created my articles by query function

--- news.py
+++ news.py
@@ -1,3 +1,4 @@
+import requests
 import creds
 
 URL = "https://newsapi.org/v2/top-headlines?"
@@ -8,6 +9,15 @@ def get_articles_by_category(category):
         "category": category,
         "country": "gb",
         "sortBy": "top",
+        "apiKey": API_KEY 
+    }
+    return _get_articles(query_params)
+
+def get_articles_by_query(query):
+    query_params = {
+        "q": query,
+        "sortBy": "top",
+        "country": "gb",
         "apiKey": API_KEY 
     }
     return _get_articles(query_params)
