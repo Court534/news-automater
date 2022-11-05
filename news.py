8e06b2c348f29d309714c76e858e149fd1fdf8de
Courtney Stow <courney-stow@hotmail.co.uk>
2022-11-05 17:49:07 +0000

    Created my articles function

--- news.py
+++ news.py
@@ -3,3 +3,11 @@ import creds
 URL = "https://newsapi.org/v2/top-headlines?"
 API_KEY = creds.API_KEY
 
+def get_articles_by_category(category):
+    query_params = {
+        "category": category,
+        "country": "gb",
+        "sortBy": "top",
+        "apiKey": API_KEY 
+    }
+    return _get_articles(query_params)
