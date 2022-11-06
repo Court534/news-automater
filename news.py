64fdba954abc5b2c90935d8cf2e83c53ddd597ef
Courtney Stow <courney-stow@hotmail.co.uk>
2022-11-06 13:07:23 +0000

    Still have a few issues returning the correct result but this is the code so far

--- news.py
+++ news.py
@@ -2,62 +2,64 @@ import requests
 from sys import argv
 import creds
 
-URL = "https://newsapi.org/v2/top-headlines?"
 API_KEY = creds.API_KEY
 
+URL = ('https://newsapi.org/v2/top-headlines?')
+
+
 def get_articles_by_category(category):
-    query_params = {
+    query_parameters = {
         "category": category,
-        "country": "gb",
         "sortBy": "top",
-        "apiKey": API_KEY 
+        "country": "gb",
+        "apiKey": API_KEY
     }
-    return _get_articles(query_params)
+    return _get_articles(query_parameters)
 
 def get_articles_by_query(query):
-    query_params = {
+    query_parameters = {
         "q": query,
         "sortBy": "top",
         "country": "gb",
-        "apiKey": API_KEY 
+        "apiKey": API_KEY
     }
-    return _get_articles(query_params)
+    return _get_articles(query_parameters)
 
 def _get_articles(params):
     response = requests.get(URL, params=params)
-    articles = response.json()["articles"]
+
+    articles = response.json()['articles']
 
     results = []
-    
+        
     for article in articles:
-        results.append({
-            "title": article["title"],
-            "url": article["url"]
-        })
-        
+        results.append({"title": article["title"], "url": article["url"]})
+
     for result in results:
-        print(result["title"])
-        print(result["url"])
-        print("")
-        
+        print(result['title'])
+        print(result['url'])
+        print('')
+
 def get_sources_by_category(category):
-    url = "https://newsapi.org/v2/top-headlines/sources"
-    query_params = {
+    url = 'https://newsapi.org/v2/top-headlines/sources'
+    query_parameters = {
         "category": category,
         "language": "en",
-        "apiKey": API_KEY 
+        "apiKey": API_KEY
     }
-    
-    response = requests.get(URL, params=query_params)
-    sources = response.json()["sources"]
-    
+
+    response = requests.get(url, params=query_parameters)
+
+    sources = response.json()['sources']
+
     for source in sources:
-        print(source["name"])
-        print(source["url"])
-        
+        print(source['name'])
+        print(source['url'])
+
+
 if __name__ == "__main__":
     print(f"Getting news for {argv[1]}...\n")
     get_articles_by_category(argv[1])
     print(f"Successfully retrieved top {argv[1]} headlines")
-        
-        
+    # get_articles_by_query("Liz Truss")
+    # get_sources_by_category("technology")
